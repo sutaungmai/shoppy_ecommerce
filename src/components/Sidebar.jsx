5b43b50b7e89a6f4d943b82964a31d910f056c4e
sutaungmai <patrickgumrai@gmail.com>
2022-09-11 15:25:25 +0700

    Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,9 +5,16 @@ import { MdOutlineCancel } from 'react-icons/md'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { links } from '../data/dummy'
+import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const activeMenu = true
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+
+  const handleCloseSideBar =() => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
@@ -17,14 +24,14 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className='flex justify-between items-center'>
-            <Link to="/" onClick={() => {}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold
+            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold
             tracking-tight dark:text-white text-slate-900">
               <SiShopware /><span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu"
             position='BottomCenter'>
               <button type='button'
-                onClick={() => {}}
+                onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
                 className="text-xl rounded-full p-3 hover:bg-light-gray
                 mt-4 block md:hidden" 
               >
@@ -42,7 +49,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) => isActive ? activeLink: normalLink}
                   >
                     {link.icon}
